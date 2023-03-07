7d29fd0f05dda2c7e6abbcea26a78d23cb298949
yasudaTomoaki <s21t335@kagawa-u.ac.jp>
2023-03-07 14:15:48 +0900

    telegram_bot.pyを修正

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,35 +1,22 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
- 
-# アクセストークン
-TOKEN = ""
- 
- 
+from telegram.ext import (Updater, CommandHandler,
+                          MessageHandler, Filters)
+
+TOKEN = "6151058622:AAGq1Yh1iuTWvcsvzMINgetlAaGPrzzQueA"
 class TelegramBot:
-    def __init__(self, system):
-        self.system = system
- 
-    def start(self, bot, update):
-        # 辞書型 inputにユーザIDを設定
-        input = {'utt': None, 'sessionId': str(update.message.from_user.id)}
- 
-        # システムからの最初の発話をinitial_messageから取得し，送信
-        update.message.reply_text(self.system.initial_message(input)["utt"])
- 
-    def message(self, bot, update):
-        # 辞書型 inputにユーザからの発話とユーザIDを設定
-        input = {'utt': update.message.text, 'sessionId': str(update.message.from_user.id)}
- 
-        # replyメソッドによりinputから発話を生成
-        system_output = self.system.reply(input)
- 
-        # 発話を送信
-        update.message.reply_text(system_output["utt"])
- 
-    def run(self):
-        updater = Updater(TOKEN)
-        dp = updater.dispatcher
-        dp.add_handler(CommandHandler("start", self.start))
-        dp.add_handler(MessageHandler(Filters.text, self.message))
-        updater.start_polling()
-        updater.idle()
-        
+  def __init__(self, system):
+    self.system = system
+  def start(self, update, bot):
+    input = {'utt':None, 'sessionId':str(update.message.from_user.id)}
+    update.message.reply_text(self.system.initial_message(input)["utt"])
+  def message(self, update, bot):
+    input = {'utt':update.message.text,
+             'sessionId':str(update.message.from_user.id)}
+    system_output = self.system.reply(input)
+    update.message.reply_text(system_output["utt"])
+  def run(self):
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", self.start))
+    dp.add_handler(MessageHandler(Filters.text, self.message))
+    updater.start_polling()
+    updater.idle()
